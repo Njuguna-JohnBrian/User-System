2657e0bbd9a2be5450ed848c81c6ec4bff5f2063
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-11-23 12:10:05 +0300

    Correct PROXY For Front-End Efficiency

--- Front End/user-system/src/services/auth.service.js
+++ Front End/user-system/src/services/auth.service.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
 
 class AuthService {
   login(username, password) {
     return axios
-      .post(API_URL + "signin", {
+      .post("/api/auth/signin", {
         username,
         password
       })
@@ -23,7 +22,7 @@ class AuthService {
   }
 
   register(username, email, password) {
-    return axios.post(API_URL + "signup", {
+    return axios.post("/api/auth/signup", {
       username,
       email,
       password
